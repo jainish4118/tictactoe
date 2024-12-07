8ea9a7c5f263f13e9c8d6eac9835d00902076554
jainish4118 <jainish.gtu.4118@gmail.com>
2024-12-07 18:54:49 +0530

    first commit

---
+++ tictac.js
@@ -0,0 +1,42 @@
+let arr = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+];
+let turn = 0;
+
+const handleclick = (row, col) => {
+    if (!arr[row][col]) {
+        arr[row][col] = turn % 2 == 0 ? "O" : "X";
+        turn = turn + 1;
+        document.getElementById("cell" + row + col).innerHTML = arr[row][col];
+        if (arr[0][0] == arr[0][1] && arr[0][1] == arr[0][2]) {
+            winner = arr[0][0];
+        }
+        else if (arr[1][0] == arr[1][1] && arr[1][1] == arr[1][2]) {
+            winner = arr[1][0];
+        }
+        else if (arr[2][0] == arr[2][1] && arr[2][1] == arr[2][2]) {
+            winner = arr[2][0];
+        }
+        else if (arr[0][0] == arr[1][0] && arr[1][0] == arr[2][0]) {
+            winner = arr[0][0];
+        }
+        else if (arr[0][1] == arr[1][1] && arr[1][1] == arr[2][1]) {
+            winner = arr[0][1];
+        }
+        else if (arr[0][2] == arr[1][2] && arr[1][2] == arr[2][2]) {
+            winner = arr[0][2];
+        }
+        else if (arr[0][2] == arr[1][1] && arr[1][1] == arr[2][0]) {
+            winner = arr[0][2];
+        }
+        else if (arr[0][0] == arr[1][1] && arr[1][1] == arr[2][2]) {
+            winner = arr[0][0];
+        }
+        console.log(winner);
+        if (winner) {
+            document.getElementById('winner').innerHTML = "winner :" + winner;
+        }
+    }
+};
